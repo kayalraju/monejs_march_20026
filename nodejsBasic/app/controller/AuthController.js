08616e92053a9e6c89e94d5ee5ed01fa635546f1
kayalraju <kayalraju003@gmail.com>
2026-04-20 14:03:15 +0530

    auth

--- nodejsBasic/app/controller/AuthController.js
+++ nodejsBasic/app/controller/AuthController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 class AuthController {
   async register(req, res) {
@@ -38,6 +39,75 @@ class AuthController {
       return res.status(500).json({
         success: true,
         message: "User created successfully",
+      });
+    }
+  }
+
+
+  async login(req,res){
+    try{
+      const {email,password}=req.body
+      if(!email || !password){
+        return res.status(400).json({
+          success: false,
+          message: "All fields are required",
+        });
+      }
+
+      const existingUser=await User.findOne({email})
+      if(!existingUser){
+       return res.status(400).json({
+        success: false,
+        message: "User not found",
+       })
+      }
+
+      const isMatch=await bcrypt.compare(password,existingUser.password)
+      if(!isMatch){
+        return res.status(400).json({
+          success: false,
+          message: "Invalid credentials",
+        });
+      }
+
+      const token= jwt.sign({
+        id:existingUser._id,
+        name:existingUser.name,
+        email:existingUser.email,
+        phone:existingUser.phone
+      },process.env.JWT_SECRECT,{expiresIn:"1d"})
+
+      return res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        data: {
+          id: existingUser._id,
+          name: existingUser.name,
+          email: existingUser.email,
+          phone: existingUser.phone,
+        },
+         token: token,
+      });
+
+    }catch(error){
+       return res.status(500).json({
+        success: true,
+        message: error.message,
+      });
+    }
+  }
+
+  async dashboard(req,res){
+    try{
+      return res.status(200).json({
+        success: true,
+        message: "welcome User dashboard",
+        dsat: req.user
+      });
+    }catch(error){
+       return res.status(500).json({
+        success: true,
+        message: error.message,
       });
     }
   }
